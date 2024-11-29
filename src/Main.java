2f3383b51d861019dade1eb4bc56262139feb0c0
NguyenHoangHao <hao.nguyenhoang.cit22@eiu.edu.vn>
2024-11-29 18:10:10 +0700

    Update print-name

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ package src;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to NguyenHoangHao's Java Repository");
+        System.out.println("Pham Van Huy");
+        System.out.println("Welcome to my repository");
     }
     
 }
