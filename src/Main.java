2a58dfbea7730ec918050aa740e08a1a1654b9d3
NguyenHoangHao <hao.nguyenhoang.cit22@eiu.edu.vn>
2024-11-29 18:20:20 +0700

    Add Main.java with welcome message

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
         System.out.println("Welcome to NguyenHoangHao's Java Repository");
         System.out.println("Pham Van Huy");
         System.out.println("Welcome to my repository");
+        System.out.println("Nguyen Tan Nam");
+        System.out.println("Welcome to my Java Repository");
     }
     
 }
